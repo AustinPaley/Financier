41c10226b46310b0241f2b6d0c69490b9eaed2d2
AustinPaley <austinpaley32@gmail.com>
2018-07-03 11:07:53 -0400

    Rendering Form on Pages

--- frontend/src/Pattern.js
+++ frontend/src/Pattern.js
@@ -3,12 +3,35 @@ import { connect } from 'react-redux'
 
 class Pattern extends React.Component{
   render(){
+    console.log(this.props.pattern)
     return(
       <div>
-        Pattern
+        {this.props.pattern !== undefined ?
+          this.props.pattern.map(pattern => {
+            return(
+              <div>
+                <div>Symbol: {pattern.symbol}</div>
+                <div>Investment Size: {pattern.investment_size}</div>
+                <div>Open: {pattern.open}</div>
+                <div>High: {pattern.high}</div>
+                <div>Low: {pattern.low}</div>
+                <div>Close: {pattern.close}</div>
+              </div>
+            )
+          })
+        :
+        null}
       </div>
     )
   }
 }
 
-export default connect()(Pattern)
+const mapStateToProps = state => {
+  if (state.pattern.patterns[0] !== undefined && state.pattern.patterns[0].patterns.payload.filter(pattern => pattern.id === parseInt(window.location.pathname.replace("/pattern/", ""))) !== null){
+    return {
+      pattern: state.pattern.patterns[0].patterns.payload.filter(pattern => pattern.id === parseInt(window.location.pathname.replace("/pattern/", "")))
+    }
+  }
+}
+
+export default connect(mapStateToProps)(Pattern)
