fd8967b93d8a3136cb2a70446a8c916a31733881
AustinPaley <austinpaley32@gmail.com>
2018-07-04 16:37:42 -0400

    Added Styling to Pattern Graphs

--- frontend/src/components/PatternChart.js
+++ frontend/src/components/PatternChart.js
@@ -7,19 +7,30 @@ export default class PatternChart extends React.Component{
 
     this.state={
       chartData: {
-        labels: this.props.pattern,
+        labels: this.props.pattern.map(day => "Day " + this.props.pattern.indexOf(day)),
         datasets: [{
+          label: "Performance Data",
+          fillColor: "rgba(66, 88, 138)",
+          strokeColor: "rgba(220,220,220,0.8)",
+          highlightFill: "rgba(90, 0, 0)",
+          highlightStroke: "rgba(90, 0, 0)",
           data: this.props.pattern,
         }]
       }
     }
   }
   render(){
+    debugger
     return(
       <div className="chart">
       <Line data={this.state.chartData}
-      width="600"
-      height="400" />
+        options={{
+          title:{
+            text: "Performance Data:"
+          }
+        }}
+        width="600"
+        height="400" />
       </div>
     )
   }
