2a1cd319866deed8b2390140cfa12d0d256424ad
AustinPaley <austinpaley32@gmail.com>
2018-07-04 12:24:17 -0400

    Fixed Navigation Bug

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -10,9 +10,9 @@ class NavBar extends React.Component{
           <div className="navigation">
             <Link to="/overview" className="todaysmarkets">Today's Markets</Link>
             <Link to="/quote-window" className="quotewindow">Quote Window</Link>
-            <Link to="stock-spotlight" className="stockspotlight">Stock Spotlight</Link>
-            <Link to="pattern-matcher" className="patternmatcher">Pattern Matcher</Link>
-            <Link to="trade" className="trade">Trade Now</Link>
+            <Link to="/stock-spotlight" className="stockspotlight">Stock Spotlight</Link>
+            <Link to="/pattern-matcher" className="patternmatcher">Pattern Matcher</Link>
+            <Link to="/trade" className="trade">Trade Now</Link>
             <Clock className="clock" format={'HH:mm:ssa'} ticking={true} timezone={'US/Eastern'} />
               {Adapter.loggedIn() ?
                 <div>
