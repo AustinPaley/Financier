bc04023e400f56438def78c6615c2c4c411ab595
AustinPaley <austinpaley32@gmail.com>
2018-07-05 18:21:13 -0400

    End of Day

--- frontend/src/reducers/pattern.js
+++ frontend/src/reducers/pattern.js
@@ -5,11 +5,9 @@ const initialState = {
 const patternReducer = (state = initialState, action) => {
   switch(action.type){
     case "ADD_PATTERN":
-    debugger
       return {...state, patterns: [...state.patterns, action.payload]}
     case "DELETE_PATTERN":
       const UpdatedPatterns = state.patterns[0].patterns.payload.filter(pattern => pattern.id !== action.payload.patterns.payload.id)
-    debugger
       return {...state, patterns: [{patterns: {payload: UpdatedPatterns}}]}
     default:
       return state;
