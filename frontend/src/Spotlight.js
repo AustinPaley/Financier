159a839e3241e77b1a6897c35dda806350803a6d
AustinPaley <austinpaley32@gmail.com>
2018-07-09 11:14:06 -0400

    Graph Renders on Spotlight Page

--- frontend/src/Spotlight.js
+++ frontend/src/Spotlight.js
@@ -2,16 +2,14 @@ import React from 'react'
 import Stock from './Stock'
 import Form from './Form'
 import StockHistoryContainer from './StockHistoryContainer'
+import SpotlightChart from './components/SpotlightChart'
 import {connect} from 'react-redux'
 import Adapter from './adapters/Adapter'
 
 const API = process.env.REACT_APP_ALPHA_VANTAGE_API
 const URL1 = "https://www.alphavantage.co/query?function"
 const DAILY = `=TIME_SERIES_DAILY`
-let SPXSYMBOL = "&symbol=SPX"
 const ONEMINUTE = "&interval=1min"
-const FINAL = URL1 + DAILY + SPXSYMBOL + ONEMINUTE + API
-const FINALSTATUS= "https://api.iextrading.com/1.0/deep/trading-status?symbols="
 
 class Spotlight extends React.Component{
   constructor(props){
@@ -59,12 +57,12 @@ class Spotlight extends React.Component{
 // <StockHistoryContainer handleDelete={this.handleDelete} historicalInfo={this.state.generalInfo["Time Series (Daily)"]} handleHistorySearch={this.handleHistorySearch} />
 
   render(){
+    console.log(this.state.symbolSearch)
     return(
       <div>
         <h2 className="stockSpotlightHeader">Stock Spotlight</h2>
         <input name="stockSearch" onChange={this.changeStock} />
         <h3>{this.state.symbolCompanyInfo.companyName}</h3>
-        <h4>Placeholder for current stock info table</h4>
         <div>Symbol</div>
         <div>{this.state.symbolQuote.symbol !== undefined ? this.state.symbolQuote.symbol : "-"}</div>
         <div>Change</div>
@@ -84,6 +82,13 @@ class Spotlight extends React.Component{
         <div>Close</div>
         <div>{this.state.symbolQuote.symbol !== undefined ? this.state.symbolQuote.close : "-"}</div>
         <button>Save This Spotlight</button>
+        <h4>Chart of Selected Stock Tabular/Graph</h4>
+        {this.state.symbolSearch !== ""
+          ?
+          <SpotlightChart primarySymbol={this.state.symbolSearch}/>
+          :
+          null
+        }
         <h4>{this.state.symbolCompanyInfo.companyName} News</h4>
         {this.state.symbolSearchNews.length !== undefined ?
           this.state.symbolSearchNews.map(newsItem => {
---
+++ frontend/src/components/SpotlightChart.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import {Line} from 'react-chartjs'
+import Adapter from '../adapters/Adapter'
+import {connect} from 'react-redux'
+const API = process.env.REACT_APP_ALPHA_VANTAGE_API
+const URL1 = "https://www.alphavantage.co/query?function"
+const DAILY = `=TIME_SERIES_DAILY`
+let SPXSYMBOL = "&symbol=SPX"
+const ONEMINUTE = "&interval=1min"
+const SPXCALL = URL1 + DAILY + SPXSYMBOL + ONEMINUTE + API
+
+class SpotlightChart extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state={
+      primarySymbol: this.props.primarySymbol,
+      chartData: {
+        labels: [],
+        datasets: [{
+          label: "Performance Data",
+          fillColor: "rgba(66, 88, 138, 0.5)",
+          strokeColor: "rgba(66, 88, 138)",
+          highlightFill: "rgba(90, 0, 0)",
+          highlightStroke: "rgba(90, 0, 0)",
+          data: [],
+        }]
+      },
+      allData: [],
+      selectedDataType: "Closes"
+    }
+  }
+
+  componentDidMount(){
+    if(this.props.primarySymbol !== ""){
+      let SYMBOL = "&symbol=" + this.props.primarySymbol
+      Adapter.makeFetch(URL1 + DAILY + SYMBOL + ONEMINUTE + API)
+      .then(res => {
+        debugger
+        this.setState({
+          chartData:{
+            labels: Object.entries(res["Time Series (Daily)"]).map(day => day[0]),
+            datasets: [{
+              label: "Performance Data",
+              fillColor: "rgba(66, 88, 138, 0.5)",
+              strokeColor: "rgba(66, 88, 138)",
+              highlightFill: "rgba(90, 0, 0)",
+              highlightStroke: "rgba(90, 0, 0)",
+              data: Object.entries(res["Time Series (Daily)"]).map(day => day[1]["4. close"])
+            }]
+          },
+          allData: res
+        })
+      })
+    }
+  }
+
+  render(){
+    console.log(this.props.primarySymbol)
+    return(
+      <div className="MatcherChart">
+        <Line data={this.state.chartData}
+          width="1200"
+          height="400" />
+      </div>
+    )
+  }
+
+}
+
+export default SpotlightChart
